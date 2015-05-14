5129a032292522c4614d2ef5497aabf5499085eb
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-05-14 17:35:07 +0200

    Adds ExtensionSupport

---
+++ src/ExtensionSupport.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the fXmlRpc Serialization package.
+ *
+ * (c) Lars Strojny <lstrojny@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace fXmlRpc\Serialization;
+
+interface ExtensionSupport
+{
+    const EXTENSION_NIL = 'nil';
+
+    /**
+     * Enables an extension
+     *
+     * @param string $extension
+     */
+    public function enableExtension($extension);
+
+    /**
+     * Disables an extension
+     *
+     * @param string $extension
+     */
+    public function disableExtension($extension);
+
+    /**
+     * Returns true if an extension is enabled
+     *
+     * @param string $extension
+     *
+     * @return boolean
+     */
+    public function isExtensionEnabled($extension);
+}
