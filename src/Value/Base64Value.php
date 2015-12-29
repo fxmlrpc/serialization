aafec8790dccbae1f2f988395cafbfc3bb01e39f
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2015-12-29 22:21:55 +0100

    Remove file header

--- src/Value/Base64Value.php
+++ src/Value/Base64Value.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the fXmlRpc Serialization package.
- *
- * (c) Lars Strojny <lstrojny@php.net>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace fXmlRpc\Serialization\Value;
 
 /**
