5843f372e5ecdfc0d4ef293862669c3e625aa7d9
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-01-01 13:55:04 +0100

    Cleanup behat feature contexts

--- features/bootstrap/ParserContext.php
+++ features/bootstrap/ParserContext.php
@@ -1,9 +1,6 @@
 <?php
 
-use Behat\Behat\Context\Context;
-use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 use Fxmlrpc\Serialization\Parser;
 use Fxmlrpc\Serialization\Value\Base64Value;
 use Webmozart\Assert\Assert;
